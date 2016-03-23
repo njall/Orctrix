341eab1b95fd8702fd7e484ae49a4f4174295f64
Niall Beard <niallbeard@gmail.com>
2016-03-23 11:25:29 +0000

    Added functions to load persons works as dictionary

--- flaskapp/orcid.py
+++ flaskapp/orcid.py
@@ -11,12 +11,16 @@ BASE_URL = "https://pub.orcid.org/"
 
 def _get_raw_json(orcid_id):
     """Get raw JSON file for orcid_id."""
-    url = BASE_URL + orcid_id
+    url = orcid_url(orcid_id)
     logging.info(url)
     resp = requests.get(url,
                         headers={'Accept':'application/orcid+json'})
 
     return resp.json()
+
+def orcid_url(orcid_id, action=""):
+    return BASE_URL + orcid_id + action
+
 
 def get_json(orcid_id):
     """Get JSON for Orcid and clean it."""
@@ -36,3 +40,26 @@ def get_json(orcid_id):
             }
 
     return json
+
+def get_works(orcid_id):
+	resp = requests.get(orcid_url(orcid_id, "/orcid-works",
+                    headers={'Accept':'application/orcid+json'})
+	orcid_data = resp.json()
+	works =  orcid_data['orcid-profile']['orcid-activities']['orcid-works']['orcid-work']
+	d = {}
+	for n,item in enumerate(works):
+	    doi, tmp_d =  dic_item(item)
+	    print(n, doi)
+	    d[doi] = tmp_d
+	return d
+
+
+def dic_item(item):
+    dobj={}
+    if item['work-external-identifiers']:
+        doi = item['work-external-identifiers']['work-external-identifier'][0]['work-external-identifier-id']['value']
+        dobj['cite'] = item['work-citation']
+        dobj['url'] = item['url']
+        return doi, dobj,
+    else:
+        return None, None
