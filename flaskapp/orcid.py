e26054506ee69e852f8bebfd388534628da11b73
Niall Beard <niallbeard@gmail.com>
2016-03-23 11:57:43 +0000

    Started reformatting profile orcid information

--- flaskapp/orcid.py
+++ flaskapp/orcid.py
@@ -9,9 +9,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 BASE_URL = "https://pub.orcid.org/"
 
-def _get_raw_json(orcid_id):
+def _get_raw_json(orcid_id, action=""):
     """Get raw JSON file for orcid_id."""
-    url = orcid_url(orcid_id)
+    url = orcid_url(orcid_id, action)
     logging.info(url)
     resp = requests.get(url,
                         headers={'Accept':'application/orcid+json'})
@@ -22,31 +22,22 @@ def orcid_url(orcid_id, action=""):
     return BASE_URL + orcid_id + action
 
 
-def get_json(orcid_id):
+def get_profile(orcid_id):
     """Get JSON for Orcid and clean it."""
     raw_json = _get_raw_json(orcid_id)
 
     # TODO Add information
-    myjson = {
-            "given_names":
-            raw_json.get("orcid-profile").get("orcid-bio").get("personal-details").get("given-names").get("value"),
-            "family_name":
-            raw_json.get("orcid-profile").get("orcid-bio").get("personal-details").get("family-name").get("value"),
-            "affiliation": None,
-            "summary": None,
-            "doiurl": None,
-            "title": None,
-            "gravatarhash": None,
-            }
-
-    return myjson
+    profile = {}
+    profile['name'] = raw_json.get("orcid-profile").get("orcid-bio").get("personal-details").get("given-names").get("value") + " " +
+                      raw_json.get("orcid-profile").get("orcid-bio").get("personal-details").get("family-name").get("value")
+    
+    return profile
 
 
 def get_works(orcid_id):
-    resp = requests.get(orcid_url(orcid_id, "/orcid-works"),
-                        headers={'Accept': 'application/orcid+json'})
-    orcid_data = resp.json()
-    works = orcid_data['orcid-profile']['orcid-activities']['orcid-works']['orcid-work']
+    """ Return dictionary containing work of person with ORCID id. Dict indexed by DOI of works """
+    raw_json = _get_raw_json(orcid_id, "orcid-works")
+    works = raw_json['orcid-profile']['orcid-activities']['orcid-works']['orcid-work']
     d = {}
     for item in works:
         doi, tmp_d = dic_item(item)
@@ -54,7 +45,7 @@ def get_works(orcid_id):
     return d
 
 
-def dic_item(item):
+def work_item(item):
     dobj={}
     if item['work-external-identifiers']:
         doi = item['work-external-identifiers']['work-external-identifier'][0]['work-external-identifier-id']['value']
@@ -63,3 +54,7 @@ def dic_item(item):
         return doi, dobj,
     else:
         return None, None
+
+def profile_item(item):
+
+
